41303e53581500e49cf236786a91e668ed74ce78
butalsoverycool <kiiim@kiiim.se>
2020-05-24 11:54:43 +0200

    hiscore success rules

--- apiRoutes/actions/type_result.js
+++ apiRoutes/actions/type_result.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
 
-const addScore = (input, callback, req, res) => {
+// return
+const print = (req, res, input) => {
 	const hiscore = require('./type_result.json');
 
-	//var currentSearchResult = 'example';
+	return res.status(200).json(input);
+};
+
+const addScore = (req, res, input) => {
+	const hiscore = require('./type_result.json');
 
 	fs.readFile(__dirname + '/type_result.json', function (err, data) {
 		var hiscore = JSON.parse(data);
@@ -14,49 +19,63 @@ const addScore = (input, callback, req, res) => {
 			(a, b) => parseFloat(a.speed) - parseFloat(b.speed)
 		);
 
-		const writeabck = JSON.stringify(hiscore, null, 2);
+		const formatted = JSON.stringify(hiscore, null, 2);
 
-		fs.writeFileSync(__dirname + '/type_result.json', writeabck, 'utf8');
+		fs.writeFileSync(__dirname + '/type_result.json', formatted, 'utf8');
 
-		callback(req, res);
+		const toPrint = {
+			YOU: { ...input, speed: input.speed + ' sec' },
+			hiscore: hiscore,
+		};
+
+		print(req, res, toPrint);
 	});
 };
 
+// take in type-result
 const type_result = (req, res) => {
-	let { input, time } = req.body;
+	let { input, speed, typer = 'unknown', id } = req.body;
+	const hiscore = require('./type_result.json');
 
-	const success = req.body.input === 'Hello World';
+	console.log(req.body);
 
-	const callback = (req, res) => {
-		const hiscore = require('./type_result.json');
+	const typeSuccess = req.body.input === 'Hello World';
 
-		return res.status(200).json({
-			success,
-			typed: input,
-			speed: `${parseFloat(req.body.time).toFixed(2)} sec`,
-			hiscore,
-		});
+	const speedSuccess =
+		hiscore.some((score) => req.body.speed < score.speed) ||
+		hiscore.length < 10;
+
+	const idSuccess = hiscore.every((score) => score.id !== id);
+
+	const success = typeSuccess && speedSuccess && idSuccess;
+
+	const result = {
+		typer,
+		success,
+		speed: `${parseFloat(speed).toFixed(2)} sec`,
+		typed: input,
+		hiscore_rank: success,
+		id,
 	};
 
-	if (!success) {
-		return callback(req, res);
-	}
-
-	const d = new Date();
-
-	addScore(
-		{
+	// if top-10, add to hiscore-list and print
+	if (success) {
+		const d = new Date();
+		addScore(req, res, {
+			typer,
 			typed: input,
-			speed: parseFloat(time).toFixed(2),
+			speed: parseFloat(speed).toFixed(2),
 			timestamp: {
 				date: d.toLocaleDateString(),
 				time: d.toLocaleTimeString(),
 			},
-		},
-		callback,
-		req,
-		res
-	);
+			id,
+		});
+	}
+
+	// if not on top-10, just print my res
+
+	print(req, res, result);
 };
 
 module.exports = type_result;
